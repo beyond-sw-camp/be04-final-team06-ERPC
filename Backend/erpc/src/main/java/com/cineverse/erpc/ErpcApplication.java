9a2c657b4c542d1c83cfebd37f514a893da4fca7
yunjaeeun <yunjaeeun12@naver.com>
2024-05-07 09:11:15 +0900

    Feat: "프로젝트 초기 설정"
    프로젝트 초기 세팅

---
+++ Backend/erpc/src/main/java/com/cineverse/erpc/ErpcApplication.java
@@ -0,0 +1,13 @@
+package com.cineverse.erpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpcApplication.class, args);
+	}
+
+}
