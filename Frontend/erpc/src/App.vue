155ce916e090a29a7082801ecdc8d657d0d37321
Jihye1101 <yang2jihye@gmail.com>
2024-05-07 10:27:40 +0900

    페이지 헤더 생성함
    페이지 헤더 생성함

--- Frontend/erpc/src/App.vue
+++ Frontend/erpc/src/App.vue
@@ -1,13 +1,18 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script setup>
-
+import Header from './components/main/Header.vue';
 </script>
 
-<style>
+<template>
+  <Header></Header>
+</template>
 
+<style>
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+}
+
+* {
+    font-family: GmarketSansMedium;
+}
 </style>
---
+++ Frontend/erpc/src/components/main/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <img src="@/assets/img/logo.png" class="logoimage">
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+* {
+    font-family: 'GmarketSansMedium';
+}
+
+body {
+    margin: 0 !important;
+}
+
+.header {
+    display: flex;
+    justify-content: center; /* 이 부분을 center로 변경 */
+    align-items: center;
+    padding: 25px 50px;
+    width: 100vw;
+    box-sizing: border-box;
+    border-bottom: 3px solid #0C2092;
+}
+
+
+.logo .logoimage {
+    display: flex;
+    align-items: center;
+    width: 100px;
+    height: auto;
+    cursor: pointer;
+}
+</style>
