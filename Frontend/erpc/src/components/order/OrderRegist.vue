662e0247512272f4975de36a022d80b44edfdc8a
Jihye1101 <yang2jihye@gmail.com>
2024-05-08 17:49:06 +0900

    수주등록페이지 표 구현함
    수주등록페이지 표 구현함

---
+++ Frontend/erpc/src/components/order/OrderRegist.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="regist-content">
+        <div class="order-regist">
+            <h1>수주 등록</h1>
+        </div>
+        <div class="list-box">
+            <table class="order-table">
+                <thead>
+                    <tr>
+                        <th>프로젝트 코드</th>
+                        <th>담당자</th>
+                        <th>거래처 코드</th>
+                        <th>거래처명</th>
+                        <th>수주 금액</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>PJ-20240508001</td>
+                        <td>유관순</td>
+                        <td>AC-20230311001</td>
+                        <td>OO상사</td>
+                        <td>15,000,000</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="order-table2">
+                <thead>
+                    <tr>
+                        <th>품목 코드</th>
+                        <th>품목 이름</th>
+                        <th>수량</th>
+                        <th>단가</th>
+                        <th>합계</th>
+                        <th>기타</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>COM-001</td>
+                        <td>LG 콤퓨타</td>
+                        <td>5</td>
+                        <td>1,800,000</td>
+                        <td>9,000,000</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.regist-content {
+    margin-top: 4%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+
+.order-regist {
+    text-align: center;
+    margin-top: 3%;
+}
+
+.list-box {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 15px;
+    margin-bottom: 100px;
+    border-radius: 10px;
+    box-sizing: border-box;
+    background-color: white;
+    height: auto;
+    width: 100%;
+    max-width: 1400px;
+    margin: 20px auto;
+    margin-bottom: 7%;
+    gap: 1px;
+}
+
+.order-table,
+.order-table2 {
+    width: 100%;
+    border-collapse: collapse;
+    margin: 20px 0;
+    font-size: 16px;
+}
+
+.order-table th,
+.order-table td,
+.order-table2 th,
+.order-table2 td {
+    text-align: center;
+    border: 1px solid #ccc;
+    padding: 8px;
+    font-family: GmarketSansMedium;
+}
+
+.order-table th,
+.order-table2 th {
+    background-color: whitesmoke;
+    color: black;
+    font-size: 18px;
+    padding: 10px;
+    height: 60px;
+}
+
+.order-table td,
+.order-table2 td {
+    height: 40px;
+}
+</style>
