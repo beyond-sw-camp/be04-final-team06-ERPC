ebdf4a0bd3d6c73b6ae7decfc00576713c24b7b1
chosoobin37 <angelajsb@naver.com>
2024-05-16 17:57:25 +0900

    Fix: Transaction Config
    Transaction config 추가 후 @Transaction 어노테이션 정상 작동 확인

---
+++ Backend/erpc/src/main/java/com/cineverse/erpc/config/TransactionConfig.java
@@ -0,0 +1,9 @@
+package com.cineverse.erpc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+}
