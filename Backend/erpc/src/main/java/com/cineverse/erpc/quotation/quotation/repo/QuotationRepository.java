580515bec1307a361dd489b96ce451879d4315e3
yunjaeeun <yunjaeeun12@naver.com>
2024-05-21 17:10:39 +0900

    feat: 견적서
    1. 견적서 코드로 견적서 조회

--- Backend/erpc/src/main/java/com/cineverse/erpc/quotation/quotation/repo/QuotationRepository.java
+++ Backend/erpc/src/main/java/com/cineverse/erpc/quotation/quotation/repo/QuotationRepository.java
@@ -4,4 +4,5 @@ import com.cineverse.erpc.quotation.quotation.aggregate.Quotation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuotationRepository extends JpaRepository<Quotation, Long> {
+    Quotation findByQuotationCode(String quotationCode);
 }
