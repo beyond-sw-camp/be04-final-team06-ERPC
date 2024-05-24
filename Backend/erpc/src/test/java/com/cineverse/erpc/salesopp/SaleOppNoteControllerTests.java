b34393181a26f54857b3b7540578a015f0852d66
chosoobin37 <angelajsb@naver.com>
2024-05-24 14:26:47 +0900

    Refactor: 영업기회 테스트 코드
    1. 공지사항 패키지 리팩토링
    2. 영업기회 및 영업기회 참고사항 테스트 패키지 생성

---
+++ Backend/erpc/src/test/java/com/cineverse/erpc/salesopp/SaleOppNoteControllerTests.java
@@ -0,0 +1,22 @@
+package com.cineverse.erpc.salesopp;
+
+import org.junit.jupiter.api.Test;
+
+class SaleOppNoteControllerTests {
+
+    @Test
+    void registSalesOppNote() {
+    }
+
+    @Test
+    void modifySalesOppNote() {
+    }
+
+    @Test
+    void deleteSalesOppNote() {
+    }
+
+    @Test
+    void findSalesOppNoteList() {
+    }
+}
---
+++ Backend/erpc/src/test/java/com/cineverse/erpc/salesopp/SalesOppControllerTests.java
@@ -0,0 +1,30 @@
+package com.cineverse.erpc.salesopp;
+
+import org.junit.jupiter.api.Test;
+
+class SalesOppControllerTests {
+
+    @Test
+    void registSalesOpp() {
+    }
+
+    @Test
+    void modifySalesOpp() {
+    }
+
+    @Test
+    void changeSalesOppStatus() {
+    }
+
+    @Test
+    void deleteSalesOpp() {
+    }
+
+    @Test
+    void findSalesOppList() {
+    }
+
+    @Test
+    void findSalesOppById() {
+    }
+}
