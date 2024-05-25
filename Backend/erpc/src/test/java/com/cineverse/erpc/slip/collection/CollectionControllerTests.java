1406097699ebcaf50d1d01e9b866bde6f75d7a98
chosoobin37 <angelajsb@naver.com>
2024-05-25 22:30:40 +0900

    Develop: 수금 테스트
    1. 수금 전체조회 테스트코드 수정

--- Backend/erpc/src/test/java/com/cineverse/erpc/slip/collection/CollectionControllerTests.java
+++ Backend/erpc/src/test/java/com/cineverse/erpc/slip/collection/CollectionControllerTests.java
@@ -1,6 +1,7 @@
 package com.cineverse.erpc.slip.collection;
 
 import com.cineverse.erpc.slip.collection.aggregate.Collection;
+import com.cineverse.erpc.slip.collection.repository.CollectionRepository;
 import com.cineverse.erpc.slip.collection.service.CollectionService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
@@ -16,18 +17,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CollectionControllerTests {
 
     private final CollectionService collectionService;
+    private final CollectionRepository collectionRepository;
 
     @Autowired
-    public CollectionControllerTests(CollectionService collectionService) {
+    public CollectionControllerTests(CollectionService collectionService,
+                                     CollectionRepository collectionRepository) {
         this.collectionService = collectionService;
+        this.collectionRepository = collectionRepository;
     }
 
     @Test
     @Transactional
     @DisplayName("수금 전체조회 테스트")
     public void findCollectionList() {
-        List<Collection> collectionList = collectionService.findCollectionList();
+        List<Collection> actualCollectionList = collectionRepository.findAll();
+        List<Collection> testCollectionList = collectionService.findCollectionList();
 
-        assertThat(collectionList).isNotEmpty();
+        assertThat(actualCollectionList.size()).isEqualTo(testCollectionList.size());
+        assertThat(testCollectionList).isNotEmpty();
     }
 }
