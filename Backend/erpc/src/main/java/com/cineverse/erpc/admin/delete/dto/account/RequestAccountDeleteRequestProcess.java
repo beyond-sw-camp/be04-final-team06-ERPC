be370f0d2049527de2c28149b8bbeb8971bb996c
yunjaeeun <yunjaeeun12@naver.com>
2024-05-27 09:02:08 +0900

    test: 삭제요청 테스트
    1. 견적서 삭제요청 전체조회 성공 테스트
    2. 견적서 삭제요청 단일조회 성공 테스트
    3. 견적서 삭제요청 처리 성공 테스트

--- Backend/erpc/src/main/java/com/cineverse/erpc/admin/delete/dto/account/RequestAccountDeleteRequestProcess.java
+++ Backend/erpc/src/main/java/com/cineverse/erpc/admin/delete/dto/account/RequestAccountDeleteRequestProcess.java
@@ -1,9 +1,11 @@
 package com.cineverse.erpc.admin.delete.dto.account;
 
 import com.cineverse.erpc.account.account.aggregate.Account;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RequestAccountDeleteRequestProcess {
     private long accountDeleteRequestId;
 }
--- Backend/erpc/src/test/java/com/cineverse/erpc/admin/delete/DeleteTests.java
+++ Backend/erpc/src/test/java/com/cineverse/erpc/admin/delete/DeleteTests.java
@@ -1,5 +1,12 @@
 package com.cineverse.erpc.admin.delete;
 
+import com.cineverse.erpc.account.account.aggregate.AccountDeleteRequest;
+import com.cineverse.erpc.account.account.repository.AccountDeleteRequestRepository;
+import com.cineverse.erpc.account.account.repository.AccountRepository;
+import com.cineverse.erpc.admin.delete.dto.account.RequestAccountDeleteRequestProcess;
+import com.cineverse.erpc.admin.delete.dto.account.ResponseAccountDeleteRequestList;
+import com.cineverse.erpc.admin.delete.dto.account.ResponseAccountDeleteRequestProcess;
+import com.cineverse.erpc.admin.delete.dto.account.ResponseFindAccoundDeleteRequest;
 import com.cineverse.erpc.admin.delete.dto.quotation.RequestQuotationDeleteRequestProcess;
 import com.cineverse.erpc.admin.delete.dto.quotation.ResponseFindQuotationDeleteRequest;
 import com.cineverse.erpc.admin.delete.dto.quotation.ResponseQuotationDeleteRequestList;
@@ -25,14 +32,20 @@ public class DeleteTests {
     private final DeleteService deleteService;
     private final QuotationDeleteRequestRepository quotationDeleteRequestRepository;
     private final QuotationRepository quotationRepository;
+    private final AccountDeleteRequestRepository accountDeleteRequestRepository;
+    private final AccountRepository accountRepository;
 
     @Autowired
     public DeleteTests(DeleteService deleteService,
                        QuotationDeleteRequestRepository quotationDeleteRequestRepository,
-                       QuotationRepository quotationRepository) {
+                       QuotationRepository quotationRepository,
+                       AccountDeleteRequestRepository accountDeleteRequestRepository,
+                       AccountRepository accountRepository) {
         this.deleteService = deleteService;
         this.quotationDeleteRequestRepository = quotationDeleteRequestRepository;
         this.quotationRepository = quotationRepository;
+        this.accountDeleteRequestRepository = accountDeleteRequestRepository;
+        this.accountRepository = accountRepository;
     }
 
     @Test
@@ -84,5 +97,51 @@ public class DeleteTests {
 
         assertThat(responseQuotationDeleteRequestProcess.getQuotationDeleteRequestStatus()).isEqualTo("Y");
         assertThat(quotation.getQuotationDeleteDate()).isNotNull();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("거래처 삭제요청 전체조회 성공 테스트")
+    public void successFindAccountDeleteRequestListTest() {
+        List<ResponseAccountDeleteRequestList> accountDeleteRequestLists =
+                deleteService.findAccountDeleteList();
+
+        assertThat(accountDeleteRequestLists).isNotEmpty();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("거래처 삭제요청 단일조회 성공 테스트")
+    public void successFindAccountDeleteRequestByIdTest() {
+        AccountDeleteRequest accountDeleteRequest = accountDeleteRequestRepository.findById(Long.valueOf(3))
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 거래처 삭제요청 입니다."));
+
+        ResponseFindAccoundDeleteRequest testAccountDeleteRequest =
+                deleteService.findAccountDeleteRequestById(3);
+
+        assertThat(accountDeleteRequest.getAccount()).isEqualTo(testAccountDeleteRequest.getAccount());
+
+        assertThat(accountDeleteRequest.getAccountDeleteRequestReason())
+                .isEqualTo(testAccountDeleteRequest.getAccountDeleteRequestReason());
+
+        assertThat(accountDeleteRequest.getAccountDeleteRequestStatus())
+                .isEqualTo(testAccountDeleteRequest.getAccountDeleteRequestStatus());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("거래처 삭제요청 처리 성공 테스트")
+    public void successAccountDeleteRequestProcessTest() {
+        RequestAccountDeleteRequestProcess accountDeleteRequestProcess
+                = RequestAccountDeleteRequestProcess.builder()
+                .accountDeleteRequestId(4)
+                .build();
+
+        ResponseAccountDeleteRequestProcess testAccountDeleteRequestProcess
+                = deleteService.accountDeleteRequestProcess(accountDeleteRequestProcess);
+
+        assertThat(testAccountDeleteRequestProcess.getAccountDeleteRequestStatus()).isEqualTo("Y");
+        assertThat(testAccountDeleteRequestProcess.getAccount().getAccountDeleteDate()).isNotNull();
+
     }
 }
