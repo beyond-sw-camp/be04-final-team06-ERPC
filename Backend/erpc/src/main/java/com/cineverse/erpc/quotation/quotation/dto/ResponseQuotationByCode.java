19803c791d0c99d61c13a1507639c73890d0c77a
yunjaeeun <yunjaeeun12@naver.com>
2024-05-28 15:40:30 +0900

    fix: 견적서 수정
    1. 견적서 코드조회 누락 해결

--- Backend/erpc/src/main/java/com/cineverse/erpc/quotation/quotation/dto/ResponseQuotationByCode.java
+++ Backend/erpc/src/main/java/com/cineverse/erpc/quotation/quotation/dto/ResponseQuotationByCode.java
@@ -2,6 +2,7 @@ package com.cineverse.erpc.quotation.quotation.dto;
 
 import com.cineverse.erpc.account.account.aggregate.Account;
 import com.cineverse.erpc.employee.aggregate.Employee;
+import com.cineverse.erpc.quotation.quotation.aggregate.QuotationProduct;
 import com.cineverse.erpc.quotation.quotation.aggregate.Transaction;
 import com.cineverse.erpc.warehouse.aggregate.Warehouse;
 import lombok.Data;
@@ -19,4 +20,5 @@ public class ResponseQuotationByCode {
     private Account account;
     private Transaction transaction;
     private Warehouse warehouse;
+    private QuotationProduct quotationProduct;
 }
